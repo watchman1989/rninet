a1b7e7bdaa32b46eac383b3b579d9a5c7b7fdf7e
Dong Yang <Dong Yang>
2019-08-29 19:02:23 +0800

    update plugin code

--- plugin/broker/kafka/kafka.go
+++ plugin/broker/kafka/kafka.go
@@ -1,1 +1,66 @@
 package kafka
+
+import (
+	"context"
+	"github.com/Shopify/sarama"
+	"github.com/watchman1989/rninet/plugin"
+	"github.com/watchman1989/rninet/plugin/broker"
+)
+
+type kafkaBroker struct {
+	options *Options
+	client sarama.Client
+}
+
+func init () {
+	plugin.InstallBrokerPlugin("kafka", NewKafkaBroker)
+}
+
+
+func NewKafkaBroker () broker.Broker {
+	return &kafkaBroker{}
+}
+
+func (b *kafkaBroker) Name() string {
+	return "kafka"
+}
+
+
+func (b *kafkaBroker) Init (ctx context.Context, opts ...interface{}) error {
+
+	b.options = &Options{}
+	for _, opt := range opts {
+		opt.(Option)(b.options)
+	}
+
+	if len(b.options.Addrs) == 0 {
+		b.options.Addrs = []string{"127.0.0.1:9092"}
+	}
+
+	return nil
+}
+
+
+func (b *kafkaBroker) Connect () error {
+
+
+	return nil
+}
+
+
+func (b *kafkaBroker) Disconnect () error {
+
+
+	return nil
+}
+
+
+func (b *kafkaBroker) Publish (topic string, m *broker.Message) {
+
+}
+
+
+func (b *kafkaBroker) Subcribe (topic string) {
+
+}
+
--- plugin/broker/kafka/options.go
+++ plugin/broker/kafka/options.go
@@ -1,6 +1,13 @@
 package kafka
 
 type Options struct {
+	Addrs []string
 }
 
 type Option func (opts *Options)
+
+func WithAddrs(addrs []string) Option {
+	return func(opts *Options) {
+		opts.Addrs = addrs
+	}
+}
